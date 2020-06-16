64408d8f382dacacafb200c98677fb4dd1e028a0
João Cardoso <joao@ip-192-168-0-17.ca-central-1.compute.internal>
2020-06-16 20:44:08 -0300

    add browser permissions

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <iframe
-          title="asd"
-          src="https://meet.jit.si/kkk"
-          width="100%"
-          height="100%"
-          frameborder="0"
-          allowfullscreen
-          sandbox
-        />
-      </header>
+    <div className="videoFrame">
+      <iframe
+        title="videoFrame"
+        src="https://meet.jit.si/rolê"
+        allow="camera;microphone"
+        allowfullscreen
+        frameborder="0"
+        height="100%"
+        width="100%"
+        sandbox
+      />
     </div>
   );
 }
