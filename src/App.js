787bf25b34b78d97c8c5878ab3d29b9c84dac67d
João Cardoso <joao@ip-192-168-0-17.ca-central-1.compute.internal>
2020-06-16 19:05:55 -0300

    initial deploy

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <iframe
+          title="asd"
+          src="https://meet.jit.si/kkk"
+          width="100%"
+          height="100%"
+          frameborder="0"
+          allowfullscreen
+          sandbox
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
