df6b1615aa63924cbb1b44780212d4417334b87e
João Cardoso <joao@MacBook-Pro-de-Franq.local>
2020-06-21 19:52:45 -0300

    add recent rooms

--- src/App.js
+++ src/App.js
@@ -1,39 +1,150 @@
 import React, { useState } from "react";
-import VideoFrameComponent from "./components/videoFrame/videoFrame";
+import {
+  SwipeableList,
+  SwipeableListItem,
+} from "@sandstreamdev/react-swipeable-list";
+import {
+  videoApi,
+  VideoFrameComponent,
+} from "./components/videoFrame/videoFrame";
+import { RecentRoomComponent } from "./components/recentRoom/recentRoom";
+import "./app.css";
 
 const App = () => {
-  const [roomName, setRoom] = useState("rolê");
-  const [displayName, setName] = useState("herói");
-  const [call, setCall] = useState(false);
+  const [displayName, setDisplayName] = useState(
+    localStorage.getItem("displayName")
+  );
+  const [loading, setLoading] = useState(false);
+  const [roomName, setRoomName] = useState(null);
+  const [newRoomName, setNewRoomName] = useState("");
+  const [recentRooms, setRecentRooms] = useState(
+    JSON.parse(localStorage.getItem("recentRooms"))
+  );
 
-  const handleClick = (event) => {
-    event.preventDefault();
-    if (roomName && displayName) setCall(true);
+  const onNameChange = (name) => {
+    localStorage.setItem("displayName", name);
+    setDisplayName(name);
+    if (videoApi) videoApi.executeCommand("displayName", name);
+  };
+
+  const onCreateRoom = () => {
+    if (newRoomName) openRoom(newRoomName);
+    setNewRoomName("");
+  };
+
+  const deleteFromRecentRooms = (roomName) => {
+    const updatetRecentRooms = recentRooms.filter((x) => x !== roomName);
+    setRecentRooms(updatetRecentRooms);
+    localStorage.setItem("recentRooms", JSON.stringify(updatetRecentRooms));
+  };
+
+  const openRoom = (roomName) => {
+    setLoading(true);
+    setRoomName(`${roomName}`);
+    updateRecentRooms(roomName);
+  };
+
+  const updateRecentRooms = (roomName) => {
+    let updatedRecentRooms;
+    if (recentRooms && recentRooms.length > 0) {
+      for (var i = 0; i < recentRooms.length; i++)
+        if (recentRooms[i] === roomName) recentRooms.splice(i, 1);
+      recentRooms.splice(0, 0, roomName);
+      updatedRecentRooms = recentRooms;
+    } else {
+      updatedRecentRooms = [roomName];
+    }
+    setRecentRooms(updatedRecentRooms);
+    localStorage.setItem("recentRooms", JSON.stringify(updatedRecentRooms));
+  };
+
+  const onLoadRoom = () => {
+    setLoading(false);
+  };
+
+  const onCloseRoom = () => {
+    setRoomName(null);
   };
 
   return (
-    <div className="container">
-      {/* <form>
-        <input
-          id="room"
-          type="text"
-          placeholder="Room"
-          value={roomName}
-          onChange={(e) => setRoom(e.target.value)}
+    <div>
+      <div className="header">
+        <div className="greetings">
+          <div className="greetingsTitle">Olá</div>
+          <form className="nameForm">
+            <input
+              className="nameInput"
+              value={displayName}
+              placeholder="Digite seu apelido"
+              onChange={(e) => onNameChange(e.target.value)}
+            />
+          </form>
+        </div>
+        {roomName && !loading && (
+          <div>
+            <div className="actualRoomTitle">você está em </div>
+            <div className="actualRoom">
+              <RecentRoomComponent roomName={roomName} />
+            </div>
+          </div>
+        )}
+        <div className="createRoomForm">
+          <input
+            value={newRoomName}
+            className="createRoomInput"
+            placeholder="Nome do rolê"
+            onChange={(e) => setNewRoomName(e.target.value)}
+          />
+          <button onClick={onCreateRoom} disabled={!newRoomName}>
+            Criar rolê
+          </button>
+        </div>
+      </div>
+      {roomName && (
+        <VideoFrameComponent
+          roomName={roomName}
+          onClose={onCloseRoom}
+          onLoad={onLoadRoom}
         />
-        <input
-
-          id="name"
-          type="text"
-          placeholder="Name"
-          value={displayName}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button onClick={handleClick} type="submit">
-          Start / Join
-        </button>
-      </form> */}
-      <VideoFrameComponent roomName={roomName} displayName={displayName} />
+      )}
+      <div className="recentRoomsList">
+        {recentRooms &&
+          recentRooms.length > 0 &&
+          recentRooms.map(
+            (recentRoomName, index) =>
+              !(roomName && index === 0) && (
+                <div className="recentRoom" key={recentRoomName}>
+                  <RecentRoomComponent
+                    roomName={recentRoomName}
+                    onClick={() => openRoom(recentRoomName)}
+                  />
+                  <button onClick={() => deleteFromRecentRooms(recentRoomName)}>
+                    Deletar
+                  </button>
+                </div>
+              )
+            // <SwipeableList>
+            //   {recentRooms.map(
+            //     (recentRoomName, index) =>
+            //       !(roomName && index === 0) && (
+            //         <SwipeableListItem
+            //           swipeLeft={{
+            //             content: <div></div>,
+            //             action: () => deleteFromRecentRooms(recentRoomName),
+            //           }}
+            //         >
+            //           <div className="recentRoom">
+            //             <RecentRoomComponent
+            //               roomName={recentRoomName}
+            //               onClick={() => openRoon(recentRoomName)}
+            //             />
+            //           </div>
+            //         </SwipeableListItem>
+            //       )
+            //   )}
+            // </SwipeableList>
+          )}
+      </div>
     </div>
   );
 };
--- src/components/videoFrame/videoFrame.js
+++ src/components/videoFrame/videoFrame.js
@@ -1,80 +1,89 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
 import "./videoFrame.css";
 
-const VideoFrameComponent = ({ roomName, displayName }) => {
-  let jitsiApi;
-  const parentNode = "jitsiContainer";
-  const onLoad = () => {};
+const domain = "meet.jit.si";
+const parentNode = "jitsiContainer";
+
+export let videoApi;
+
+export const VideoFrameComponent = ({ roomName, onClose, onLoad }) => {
+  const [loading, setLoading] = useState(true);
+  const displayName = localStorage.getItem("displayName");
 
   const hangUp = () => {
-    jitsiApi.executeCommand("hangup");
+    if (videoApi) videoApi.dispose();
+    if (onClose) onClose();
+  };
+
+  const onJoined = () => {
+    setLoading(false);
+    if (onLoad) onLoad();
   };
 
   useEffect(() => {
-    const domain = "meet.jit.si";
+    if (!loading) setLoading(true);
+    //if (videoApi) videoApi.dispose();
     const options = {
-      onLoad,
       roomName,
-      // width: "100%",
-      // height: "100%",
       parentNode: document.querySelector(`#${parentNode}`),
       userInfo: {
         displayName: displayName,
       },
       configOverwrite: {
-        enableNoAudioDetection: false,
-        enableNoisyMicDetection: false,
-        startWithAudioMuted: true,
         resolution: 240,
+        disableDeepLinking: true,
+        disableAudioLevels: true,
+        startWithAudioMuted: true,
+        enableTalkWhileMuted: false,
+        disableRemoteMute: true,
+        remoteVideoMenu: {
+          disableKick: true,
+        },
+        p2p: {
+          enabled: true,
+          stunServers: [{ urls: "stun:meet-jit-si-turnrelay.jitsi.net:443" }],
+        },
       },
       interfaceConfigOverwrite: {
+        SHOW_JITSI_WATERMARK: true,
+        DEFAULT_LOGO_URL: "../images/watermark.png",
+        JITSI_WATERMARK_LINK: " ",
         DEFAULT_BACKGROUND: "white",
-        DISABLE_VIDEO_BACKGROUND: true,
         DEFAULT_LOCAL_DISPLAY_NAME: "você",
         DEFAULT_REMOTE_DISPLAY_NAME: null,
-        SHOW_JITSI_WATERMARK: false,
-        TOOLBAR_BUTTONS: [
-          "microphone",
-          "camera",
-          //   "closedcaptions",
-          //   "desktop",
-          "fullscreen",
-          //   "fodeviceselection",
-          //   "hangup",
-          //   "profile",
-          "chat",
-          //   "recording",
-          //   "livestreaming",
-          // "etherpad",
-          // "sharedvideo",
-          // "settings",
-          // "raisehand",
-          // "videoquality",
-          // "filmstrip",
-          // "invite",
-          // "feedback",
-          // "stats",
-          // "shortcuts",
-          // "tileview",
-          // "videobackgroundblur",
-          // "download",
-          // "help",
-          // "mute-everyone",
-          // "e2ee",
-          // "security",
-        ],
-        // filmStripOnly: true,
         VIDEO_QUALITY_LABEL_DISABLED: true,
-        CONNECTION_INDICATOR_DISABLED: true,
-        //VERTICAL_FILMSTRIP: true,
+        TOOLBAR_BUTTONS: [
+          "chat",
+          "camera",
+          "hangup",
+          "microphone",
+          "fullscreen",
+          "sharedvideo",
+        ],
       },
     };
     // eslint-disable-next-line no-undef
-    jitsiApi = new JitsiMeetExternalAPI(domain, options);
-    jitsiApi.executeCommand("toggleTileView");
+    videoApi = new JitsiMeetExternalAPI(domain, options);
+    videoApi.addEventListener("videoConferenceJoined", onJoined);
+    videoApi.addEventListener("readyToClose", hangUp);
+    videoApi.executeCommand("toggleFilmStrip");
+
+    videoApi.executeCommand("subject", " ");
   }, [roomName]);
 
-  return <div id={parentNode} className="jitsiContainer" />;
+  return (
+    <div>
+      <PacmanLoader
+        loading={loading}
+        color="#424242"
+        size="6vh"
+        css="height: 60vh; margin-left: 20px;"
+      />
+      <div
+        id={parentNode}
+        className={loading ? "hiddenContainer" : "visibleContainer"}
+      />
+    </div>
+  );
 };
-
-export default VideoFrameComponent;
